3e1fafd4e36c80b58a135642e33d6e0bfbc73228
Facundo Velazco <elfacu@MBP-de-Facundo.fibertel.com.ar>
2022-09-08 11:16:35 -0300

    mejorando codigo

--- Mastermind/appv0.js
+++ Mastermind/appv0.js
@@ -1,91 +1,39 @@
-/**
- * Mastermind version orientada a procesos
- * Curso de Escuela IT
- * 
- * Suposiciones
- * - Los colores son {RED,BLUE,GREEN,YELLOW,CYAN,MAGENTA}
- * - En cada juego se genera una combinacion de colores aleatoria.
- *
- */
-
 const { Console } = require("../console");
 
 const console = new Console();
 
-let option;
+initGame();
 
-do{
-    console.writeln("JUGUEMOS MASTERMIND!!!")
-    console.writeln("    [y] - SI ")
-    console.writeln("     ...presione otra tecla para salir")
-
-    option = console.readString("ingrese respuesta...");
-
-    if(option === 'y') playMastermind();
-
-}while(option === 'y');
-
-console.writeln("ADIOSSS!!!")
-
+function initGame(){
+    let option;
+    do{
+        option = printMenu();
+        if(option) 
+            playMastermind();
+    }while(option);
+    console.writeln("ADIOSSS!!!")
+}
 function playMastermind(){
     const COLORS = ['r','b','g','y','c','m'];
     const CHANCES = 10;
     printRules();
-
+    
     const secretColors = genSecretColors(COLORS);
     
     console.writeln("EMPECEMOS A JUGAR!!")
-
+    
     let moves = 0, win;
     do{
         let input = getInput(COLORS);
-        
         printBoard(input,secretColors);
-
         win = compare(secretColors,input);
-
         moves++;
-    }while(moves < CHANCES && !win) 
+    }while(moves < CHANCES && !win)
 
-    console.writeln("JUEGO TERMINADO!!!")
-    if(win){
-        console.writeln("El jugador gana!!! :D")
-    }else{
-        console.writeln("El jugador pierde!!! :D")
-
-        console.writeln(`La palabra secreta era ${secretColors}`)
-    }
-
-    console.writeln("-----------------------------------------------")
-}
-function compare(secretColors,input){
-    let equals = true;
-    for(let i =0; equals && i < secretColors.length; i++){
-        equals &&= secretColors[i] === input[i];
-    }
-    return equals;
-}
-function printBoard(input,secretColors){
-    let answer = ['-','-','-','-'];
-    for(let i=0; i < 4; i++){
-        if(secretColors.includes(input[i])){
-            answer[i] = 'O'    
-            if(secretColors[i] === input[i]){
-                answer[i] = 'X'
-            }
-        }
-    }
-    
-    let shuffledAnswer = answer.sort(function () {return Math.random() - 0.5;});
-    
-    console.writeln("-----------------------------------------------")
-    console.writeln(`                  ${input} || ${shuffledAnswer}`)
-    console.writeln("-----------------------------------------------")
-
+    gameFinished(win,secretColors);
 }
 function genSecretColors(colors){
     let secretColors = [];
-
     for(let i = 0; i < 4; i++){
         let randomIndex = parseInt(Math.random() * colors.length);
         secretColors[i] = colors[randomIndex];
@@ -97,19 +45,63 @@ function getInput(colors){
     do{
         ok = true;
         input = console.readString("Esperando respuesta...");
-
+        
         if(input.length !== 4){
             console.writeln("ERROR! Se deben ingresar 4 letras!");
             ok = false;
         }
         for(let i = 0; ok && i < 4; i++){
             ok &&= colors.includes(input[i]);
-
-            if(!ok) console.writeln("ERROR! Los colores permitidos son 'r','b','g','y','c','m'");  
+            if(!ok) 
+            console.writeln("ERROR! Los colores permitidos son 'r','b','g','y','c','m'");  
         }
     }while(!ok);
-
+    
     return input;
+}
+function compare(secretColors,input){
+    let equals = true;
+    for(let i =0; equals && i < secretColors.length; i++)
+    equals &&= secretColors[i] === input[i];
+    return equals;
+}
+function printBoard(input,secretColors){
+    const INCLUDED_SYMBOL = 'O';
+    const SUCCESS_SYMBOL = 'X';
+    const EMPTY_SYMBOL = '-';
+    const answer = [EMPTY_SYMBOL,EMPTY_SYMBOL,EMPTY_SYMBOL,EMPTY_SYMBOL];
+    for(let i=0; i < 4; i++){
+        if(secretColors.includes(input[i])){
+            answer[i] = INCLUDED_SYMBOL;    
+            if(secretColors[i] === input[i]){
+                answer[i] = SUCCESS_SYMBOL;
+            }
+        }
+    }
+    let shuffledAnswer = answer.sort(function () {return Math.random() - 0.5;});
+    console.writeln("-----------------------------------------------")
+    console.writeln(`                  ${input} || ${shuffledAnswer}`)
+    console.writeln("-----------------------------------------------")
+}
+function gameFinished(win,secretColors){
+    console.writeln("JUEGO TERMINADO!!!")
+    if(win){
+        console.writeln("El jugador gana!!! :D")
+    }else{
+        console.writeln("El jugador pierde!!! :-(")
+        console.writeln(`La palabra secreta era ${secretColors}`)
+    }
+    console.writeln("-----------------------------------------------")
+}
+function printMenu(){
+    let rtn = true, option;
+    console.writeln("JUGUEMOS MASTERMIND!!!");
+    console.writeln("    [y] - SI ");
+    console.writeln("     ...presione otra tecla para salir");
+    option = console.readString("ingrese respuesta...");
+    if(option !== 'y')
+        rtn = false;
+    return rtn;
 }
 function printRules(){
     console.writeln("-----------------------------------------------")
@@ -118,5 +110,3 @@ function printRules(){
     console.writeln("\nSe devolvera el caracter X por cada acierto en \ncolor y posicion y O por cada acierto en color")
     console.writeln("-----------------------------------------------")
 }
-
-
--- Mastermind/test.js
+++
@@ -1,8 +0,0 @@
-let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
-
-let shuffledNumbers = numbers.sort(function () {
-  return Math.random() - 0.5;
-});
-
-console.log(shuffledNumbers);
-// [7, 8, 3, 1, 5, 4, 2, 6]
